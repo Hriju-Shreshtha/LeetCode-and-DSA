09c9fd9697b3682d985bc3bdcc908eaee239dd0f
Hriju Shreshtha <49452078+Hriju-Shreshtha@users.noreply.github.com>
2025-03-11 15:56:03 +0530

    Time: 4 ms (88.64%), Space: 50.9 MB (22.03%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -1,27 +1,47 @@
+// class Solution {
+//     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+//         int count=0;
+//         if (ruleKey.equalsIgnoreCase("type")){
+//             for(List<String> elements: items){
+//                 if(elements.get(0).equalsIgnoreCase(ruleValue)){
+//                     count++;
+//                 }
+//             }
+//         }
+//         else if (ruleKey.equalsIgnoreCase("color")){
+//             for(List<String> elements: items){
+//                 if(elements.get(1).equalsIgnoreCase(ruleValue)){
+//                     count++;
+//                 }
+//             }
+//         }
+//         else if (ruleKey.equalsIgnoreCase("name")){
+//             for(List<String> elements: items){
+//                 if(elements.get(2).equalsIgnoreCase(ruleValue)){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
+
 class Solution {
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-        int count=0;
-        if (ruleKey.equalsIgnoreCase("type")){
-            for(List<String> elements: items){
-                if(elements.get(0).equalsIgnoreCase(ruleValue)){
-                    count++;
-                }
-            }
+     int key = 0; 
+    int count = 0;
+
+    switch (ruleKey) {
+        case "type": key = 0; break;
+        case "color": key = 1; break;
+        case "name": key = 2; break;
+    }
+
+    for (List<String> item : items) {
+        if (item.get(key).equals(ruleValue)) {
+            count++;
         }
-        else if (ruleKey.equalsIgnoreCase("color")){
-            for(List<String> elements: items){
-                if(elements.get(1).equalsIgnoreCase(ruleValue)){
-                    count++;
-                }
-            }
-        }
-        else if (ruleKey.equalsIgnoreCase("name")){
-            for(List<String> elements: items){
-                if(elements.get(2).equalsIgnoreCase(ruleValue)){
-                    count++;
-                }
-            }
-        }
-        return count;
+    }
+    return count;
     }
 }

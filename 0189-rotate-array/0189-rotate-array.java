5aaecb2252fd42d809692b9303b181ab1d8d8826
Hriju Shreshtha <49452078+Hriju-Shreshtha@users.noreply.github.com>
2025-03-22 21:26:15 +0530

    Update 0189-rotate-array.java

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,6 +1,8 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         k=k%nums.length;
+
+        // rotating array k time is equal to reversing the arrat entirely then reversing the first k elements of the arrar then reversing the rest of the arrat elements
         rev(nums,0, nums.length-1);
         rev(nums,0,k-1);
         rev(nums,k,nums.length-1);

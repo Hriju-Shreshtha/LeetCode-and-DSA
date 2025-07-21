130fe644682a40afba8be84355be2a654a2ea48d
Hriju Shreshtha <49452078+Hriju-Shreshtha@users.noreply.github.com>
2025-07-21 21:30:34 +0530

    Time: 1 ms (100%), Space: 45.4 MB (6.47%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.java
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.java
@@ -1,14 +1,27 @@
-import java.util.*;
 class Solution {
     public int[] transformArray(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==0){   // check the number is even 
-                nums[i] = 0;    // store 0 in their index
-            }else{
-                nums[i] = 1;   // if the number is odd then store 1 in the index
+        int evenCount=0,oddCount=0;
+        int arr[]=new int[nums.length];
+        int k=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]%2==0)
+            {
+              evenCount++;
+            }
+            else
+            {
+              oddCount++;
             }
         }
-        Arrays.sort(nums);  // sort the array
-        return nums;        // return the array
+        for(int i=1;i<=evenCount;i++)
+        {
+           arr[k++]=0;
+        }
+         for(int i=1;i<=oddCount;i++)
+        {
+           arr[k++]=1;
+        }
+        return arr;
     }
 }

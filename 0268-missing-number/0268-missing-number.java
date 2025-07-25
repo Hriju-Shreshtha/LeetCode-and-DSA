6588d991fcb6c590a6613dc31cbf84df90808bc5
Hriju Shreshtha <49452078+Hriju-Shreshtha@users.noreply.github.com>
2025-07-25 22:50:15 +0530

    Time: 0 ms (100%), Space: 45.5 MB (30.79%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -2,46 +2,46 @@
 
 // CYCLIC SORT 
 
-class Solution {
-    public static int missingNumber(int[] arr) {
-
-        // List<Integer> ans = new ArrayList<Integer>();
-        int i=0;
-        while(i<arr.length){
-            int correct = arr[i];
-            if (arr[i] < arr.length && arr[i] != arr[correct]){
-                swap(arr, i, correct);
-            }
-            else{
-                i++;
-            }
-        }
-
-        for(int j=0; j<arr.length; j++){
-            if (arr[j]!= j){
-                return j;
-            }
-        }
-
-        return arr.length;
-    }
-
-
-    static void swap(int[] arr, int first, int second) {
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
-    }
-}
-
 // class Solution {
-//     public int missingNumber(int[] nums) {
-//         int n=nums.length;
-//         int sum=(n*(n+1))/2;
-//         for(int i=0;i<n;i++)
-//         {
-//             sum-=nums[i];
+//     public static int missingNumber(int[] arr) {
+
+//         // List<Integer> ans = new ArrayList<Integer>();
+//         int i=0;
+//         while(i<arr.length){
+//             int correct = arr[i];
+//             if (arr[i] < arr.length && arr[i] != arr[correct]){
+//                 swap(arr, i, correct);
+//             }
+//             else{
+//                 i++;
+//             }
 //         }
-//         return sum;
+
+//         for(int j=0; j<arr.length; j++){
+//             if (arr[j]!= j){
+//                 return j;
+//             }
+//         }
+
+//         return arr.length;
+//     }
+
+
+//     static void swap(int[] arr, int first, int second) {
+//         int temp = arr[first];
+//         arr[first] = arr[second];
+//         arr[second] = temp;
 //     }
 // }
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int sum=(n*(n+1))/2;
+        for(int i=0;i<n;i++)
+        {
+            sum-=nums[i];
+        }
+        return sum;
+    }
+}

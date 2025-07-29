82de88f8fc60666fbcc8ada049206f37f2995aa8
Hriju Shreshtha <49452078+Hriju-Shreshtha@users.noreply.github.com>
2025-07-29 05:47:46 +0530

    Time: 2 ms (98.96%), Space: 45 MB (46.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-         Map<Integer, Integer> complements = new HashMap<>();
-        for(int i= 0; i < nums.length; i++ ){
-            Integer complementIndex = complements.get(nums[i]); // we are using the wrapper class as we cannot perform the null check with the integer datatype and we cannot use the default value to check if the complement exists as 0 might be a possible solution.
-            if(complementIndex != null){
-                return new int []{i, complementIndex};
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                return new int []{map.get(target-nums[i]),i};
             }
-             complements.put(target-nums[i],i);
+                map.put(nums[i],i);
         }
-        return nums;
-        
-}
+        return new int[]{-1,-1};
+    }
 }
